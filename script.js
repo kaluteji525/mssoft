47f2b724db4f623e45f136eb630f31e45165343d
kaluteji525 <kaluteji52@gmail.com>
2024-07-19 20:12:32 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function autoDial() {
-  var phoneNumber = "+1 (800) 642-7676";
-  window.location.href = "tel:" + phoneNumber;
+  const phoneNumber = "+1 (800) 642-7676";
+  const userConfirmed = confirm(`Would you like to call ${phoneNumber}?`);
+  if (userConfirmed) {
+    window.location.href = `tel:${phoneNumber}`;
+  }
 }
+
+window.onload = autoDial;
